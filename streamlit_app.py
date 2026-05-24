9c127d82618b9462e41df7039008b295514fd7b6
KASHAN425 <136155425+KASHAN425@users.noreply.github.com>
2026-05-25 02:00:02 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -154,7 +154,7 @@ def prediction_form() -> None:
             response = requests.post(f"{API_URL}/predict", json=payload, timeout=10)
             response.raise_for_status()
             result = response.json()
-            if result['prediction'] == 0:
+            if result['prediction'] == "0":
                 st.success("Predicted income : >=50")
             else: 
                 st.success(f"Predicted income: {result['prediction']}")
